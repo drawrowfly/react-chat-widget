eb9396011daef0bf623ccf8e60bd3f9f24ef929e
JACKASS <anonim.megamim@gmail.com>
2019-11-09 17:44:39 +0100

    ,

--- src/index.js
+++ src/index.js
@@ -4,4 +4,26 @@ import ChatWidget from './components/chat'
 
 let init = null;
 
-            ReactDOM.render(<ChatWidget />, document.querySelector('#chat-app'));
+export default {
+  config: (config) =>{
+      init = config;
+  },
+  widgets: {
+    myWidget: {
+      new: () => {
+        return {
+          render: () => {
+            ReactDOM.render(<ChatWidget 
+                clientKey={init.apiKey}
+                headerTitle={init.headerTitle}
+                selector={init.selector}  
+            />, document.querySelector('#chat-app'));
+          },
+          unmount(){
+            ReactDOM.unmountComponentAtNode(document.querySelector(init.selector)); 
+          },
+        }
+      }
+    }
+  }
+}
